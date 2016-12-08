a85d4540e3be0da3e2bf034cee47b0f1d56e1ed7
Jeff Kereakoglow <jkereako@users.noreply.github.com>
2016-12-08 14:30:15 -0500

    Save scene

--- Assets/ClockAnimator.cs
+++ Assets/ClockAnimator.cs
@@ -1,9 +1,14 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ClockAnimator : MonoBehaviour {
 	public Transform hours, minutes, seconds;
+	private const float
+	hoursToDegrees = 360f / 12f,
+	minutesToDegrees = 360f / 60f,
+	secondsToDegrees = 360f / 60f;
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +17,9 @@ public class ClockAnimator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		DateTime time = DateTime.Now;
+		hours.localRotation = Quaternion.Euler(0f, 0f, time.Hour * -hoursToDegrees);
+		minutes.localRotation = Quaternion.Euler(0f, 0f, time.Minute * -minutesToDegrees);
+		seconds.localRotation = Quaternion.Euler(0f, 0f, time.Second * -secondsToDegrees);
 	}
 }
